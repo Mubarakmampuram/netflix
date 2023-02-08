80cbc4de33de21a309b740f9c3461c2e3f6aa9b1
NTPL <mubarakmampuram@gmail.com>
2023-02-08 12:10:35 +0530

    netflix

---
+++ netflix/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import './App.css'
+import Banner from "./components/Banner/Banner";
+import Rowpost from "./components/RowPost/Rowpost";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Banner />
+     <Rowpost />
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "90d985ec5ad53ff206bfe81b4e3715ae"
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
